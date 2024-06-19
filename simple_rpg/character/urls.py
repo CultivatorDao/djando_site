cc7e705b9ad939f1f086c2fbf1a1d14bd683ad8d
Hessian <cultivatordao@gmail.com>
2024-06-19 10:05:31 +0300

    V 0.0.2 Added view function for character profile. Created simple interface for character app.

--- simple_rpg/character/urls.py
+++ simple_rpg/character/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name="main_page")
+    path('', views.index, name="character_profile")
 ]
--- simple_rpg/character/views.py
+++ simple_rpg/character/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    data = {
+        'name': 'Name',
+        'gold': 0
+    }
+    return render(request, 'character/index.html', context=data)
