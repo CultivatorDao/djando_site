42ce4bb2a36221e6a17067e4c0a5aa7048498b62
Hessian <cultivatordao@gmail.com>
2024-06-20 10:37:39 +0300

    V 0.0.4 Created model Character for player character information.

--- simple_rpg/character/models.py
+++ simple_rpg/character/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Character(models.Model):
+    name = models.CharField(max_length=20)
+    max_hp = models.IntegerField(default=100)
+    current_hp = models.IntegerField(default=100)
+    damage = models.IntegerField(default=10)
+    defence = models.IntegerField(default=5)
