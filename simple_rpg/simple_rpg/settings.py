c772de3f7894fd76d41fbc90f4cd6a499318c101
Hessian <cultivatordao@gmail.com>
2024-06-23 14:04:44 +0300

    V 0.0.1 Created new app Arena. Created template for app Arena.

--- simple_rpg/simple_rpg/settings.py
+++ simple_rpg/simple_rpg/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main_page.apps.MainPageConfig',
-    'character.apps.CharacterConfig'
+    'character.apps.CharacterConfig',
+    'arena.apps.ArenaConfig'
 ]
 
 MIDDLEWARE = [
--- simple_rpg/simple_rpg/urls.py
+++ simple_rpg/simple_rpg/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main_page.urls')),
-    path('character/', include('character.urls'))
+    path('character/', include('character.urls')),
+    path('arena/', include('arena.urls'))
 ]
