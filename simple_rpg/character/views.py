91fd5562dfe581f6fe57b1e5be82800f7134a77a
Hessian <cultivatordao@gmail.com>
2024-06-23 13:56:30 +0300

    V 0.0.5 Character model is now used to display information on character page.

--- simple_rpg/character/views.py
+++ simple_rpg/character/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
+from .models import Character
 
 
 def index(request):
+    charInfo = Character.objects.all()[0]
     data = {
         'name': 'Name',
-        'gold': 0
+        'gold': 0,
+        'stats': [
+            'Name', 'Gold', 'Health', 'Damage', 'Defence'
+        ],
+        'values': {
+            'Name': charInfo.name,
+            'Gold': 0,
+            'Health': [charInfo.current_hp, charInfo.max_hp],
+            'Damage': charInfo.damage,
+            'Defence': charInfo.defence
+        },
     }
+
     return render(request, 'character/index.html', context=data)
