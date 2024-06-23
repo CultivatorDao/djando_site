d11270cb20cc9615c59dd348b2a92dc98d72c911
Hessian <cultivatordao@gmail.com>
2024-06-23 14:30:49 +0300

    V 0.0.2 Added basic style for Arena. Created model Enemy that contains information about enemies.

--- simple_rpg/arena/models.py
+++ simple_rpg/arena/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Enemy(models.Model):
+    name = models.CharField(max_length=255)
+    health = models.IntegerField(default=100)
+    damage = models.IntegerField(default=10)
+    defence = models.IntegerField(default=5)
+    gold_reward = models.IntegerField(default=0)
--- simple_rpg/arena/views.py
+++ simple_rpg/arena/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
 
+import random
+
+from .models import Enemy
+from character.models import Character
+
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'arena/index.html')
+    enemy = Enemy.objects.all()[0]
+    player = Character.objects.get(pk=1)
+    data = {
+        'enemy': {
+            'name': enemy.name,
+            'health': enemy.health,
+            'damage': enemy.damage,
+            'defence': enemy.defence
+        },
+        'character': {
+            'name': player.name,
+            'health': player.max_hp,
+            'damage': player.damage,
+            'defence': player.defence
+        }
+    }
+    return render(request, 'arena/index.html', data)
