27017640b473c1a9c2ab3d12f0a1335bbf08b4b0
Hessian <cultivatordao@gmail.com>
2024-07-02 18:23:24 +0300

    V 0.0.6 Minor changes in static files. Added "+" button to increase stats. Added feature: Increase stats.

--- simple_rpg/character/urls.py
+++ simple_rpg/character/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name="character_profile")
+    path('', views.index, name="character_profile"),
+    path('increase_stat/', views.increase_stats, name="increase_stat")
 ]
