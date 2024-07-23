7c8136adb50ecbf4ab8c0d63abbcf4b7490ce149
Hessian <cultivatordao@gmail.com>
2024-07-23 09:02:18 +0300

    V 0.0.6 View "colosseum battle" now divided on 3 sections:     'Prepare' - clears old battles of player and creates new.     'Battle' - all battle logic take place here     'Result' - calculates rewards after battle (WIP) Some link were changed from absolute path to relative in templates.

--- simple_rpg/arena/views.py
+++ simple_rpg/arena/views.py
@@ -48,6 +48,7 @@ def colosseum(request):
     enemies = StrongEnemy.objects.all()
 
     data = {}
+    next_url = reverse('colosseum_battle', args=["prepare"])
 
     for enemy in enemies:
         data[enemy.name] = battle_manager.get_info(
@@ -56,13 +57,14 @@ def colosseum(request):
             },
             enemy=enemy
         )['enemy']
+        # data[enemy.name]['next'] = f"{next_url}?id={data[enemy.name]['id']}"
+        data[enemy.name]['next'] = f"prepare?id={data[enemy.name]['id']}"
 
     return render(
         request,
         'arena/colosseum.html',
         {
             'enemies': data,
-            'next': reverse('colosseum_battle', args=["battle"])
         }
     )
 
@@ -70,23 +72,56 @@ def colosseum(request):
 def colosseum_battle(request, state):
     player = Character.objects.get(pk=1)
     enemy = StrongEnemy.objects.get(pk=1)
+    battle = player.battles.all()[0]
 
-    # Look if there is any battle with this character
-    # In future character will have only 1 battle ongoing
-    # If battle unfinished delete all information about this battle and create new
-    if player.battles.all():
-        battle = player.battles.all()[0]
-    else:
-        battle = ColosseumBattle(
-            player=player,
-            enemy_data=json.dumps(battle_manager.get_info(
-                options={
-                    'except': ['_state', 'id']
-                },
-                enemy=enemy
-            )['enemy'])
-        )
-        battle.save()
+    match state:
+        case 'prepare':
+
+            if request.GET:
+                enemy = StrongEnemy.objects.get(pk=request.GET['id'])
+
+            # Look if there is any battle with this character
+            # In future character will have only 1 battle ongoing
+            # If battle unfinished delete all information about this battle and create new
+            if player.battles.all():
+                player.battles.all()[0].delete()
+
+            battle = ColosseumBattle(
+                player=player,
+                enemy_data=json.dumps(battle_manager.get_info(
+                    options={
+                        'except': ['_state', 'id']
+                    },
+                    enemy=enemy
+                )['enemy'])
+            )
+            battle.save()
+
+            return redirect(reverse('colosseum_battle', args=["battle"]))
+
+        case 'battle':
+
+            data = battle_manager.colosseum_fight_calculate(battle)
+            data["next"] = reverse('colosseum_battle', args=["battle"])
+            data["attack"] = random.randint(0, 10)
+            if request.GET:
+                if request.GET["attack"]:
+                    battle.enemy['health'] -= 10
+            print(battle.enemy['health'])
+
+            battle.save()
+
+            match data["status"]:
+                case "Finished":
+                    return redirect(reverse('colosseum_battle', args=["result"]))
+                case _:
+                    return render(request, 'arena/colosseum_battle.html', data)
+
+        case 'result':
+
+            data = {}
+
+            return render(request, 'arena/colosseum_battle_result.html', data)
 
     data = battle_manager.colosseum_fight_calculate(battle)
 
